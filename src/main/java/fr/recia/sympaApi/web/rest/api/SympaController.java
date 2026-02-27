d9c00b42740488687489e0017e8c120205c60e60
Ewann ROUX <ewann.roux@recia.fr>
2026-02-27 11:55:49 +0100

    fix: rename list endpoint to lists endpoint in SympaController and change method from GET to POST

--- src/main/java/fr/recia/sympaApi/web/rest/api/SympaController.java
+++ src/main/java/fr/recia/sympaApi/web/rest/api/SympaController.java
@@ -26,6 +26,7 @@ import fr.recia.sympaApi.utils.UserAttributesHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,7 +46,7 @@ public class SympaController {
   @Autowired
   UserAttributesHandler userAttributesHandler;
 
-  @GetMapping("/list")
+  @PostMapping("/lists")
   public ResponseEntity<SympaListResponseForDisplay> fetchSympaList( @RequestBody(required = false) SympaListRequestForm sympaListRequestForm) throws Exception {
 
   if(Objects.isNull(sympaListRequestForm)){
